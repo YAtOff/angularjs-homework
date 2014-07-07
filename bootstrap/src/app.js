0187118570dcd709f0492974bd2726a744b1a4c8
Yavor Atov <yavor.atov@gmail.com>
2014-07-07 09:38:41 +0300

    Added ngl-repeat directive and make it works

--- bootstrap/src/app.js
+++ bootstrap/src/app.js
@@ -44,6 +44,7 @@ Provider.directive('ngl-bind', function () {
             scope.$watch(exp, function () {
                 el.textContent = scope.$eval(exp);
             }); 
+            el.textContent = scope.$eval(exp);
         }
     };
 });
@@ -51,17 +52,44 @@ Provider.directive('ngl-bind', function () {
 Provider.directive('ngl-repeat', function () {
     'use strict';
     return {
-        scope: true,
+        scope: false,
         link: function (el, scope, exp) {
+            var parent = el.parentNode,
+                itemElementTemplate,
+                itemName, itemElement,
+                itemsExp, items,
+                newScope,
+                groups, i;
+
             // clone this node without ngl-repeat
+            itemElementTemplate = el.cloneNode();
+            itemElementTemplate.removeAttribute('ngl-repeat');
             // get item and items parts
-            // add watche for items with callback
-                // remove existing items
-                // for every item
-                // add node to parent
-                // create new scope
-                // put item in it
-                // compile node with new scope
+            groups = exp.match(/([a-zA-Z_$][0-9a-zA-Z_$]*)\s+in\s+(.*)/);
+            if (!!groups) {
+                itemName = groups[1];
+                itemsExp = groups[2];
+                // add watch for items
+                scope.$watch(itemsExp, function() {
+                    // remove existing elements
+                    while (parent.firstChild) {
+                        parent.removeChild(parent.firstChild);
+                    }
+                    // for every item
+                    items = scope.$eval(itemsExp);
+                    for (i = 0; i < items.length; i += 1) {
+                        // add node to parent
+                        itemElement = itemElementTemplate.cloneNode();
+                        parent.appendChild(itemElement);
+                        // create new scope
+                        newScope = scope.$new();
+                        // put item in it
+                        newScope[itemName] = items[i];
+                        // compile node with new scope
+                        DOMCompiler.compile(itemElement, newScope);
+                    }
+                });
+            }
         }
     };
 });
@@ -69,9 +97,11 @@ Provider.directive('ngl-repeat', function () {
 Provider.controller('TodoCtrl', function () {
     'use strict';
     return function(scope) {
-        scope.todo = 'TODO';
-        scope.foo = function() {
-            console.log('foo');
+        scope.todos = [
+        ];
+        scope.todo = '';
+        scope.add = function() {
+            scope.todos.push(scope.todo);
         };
     };
 });
