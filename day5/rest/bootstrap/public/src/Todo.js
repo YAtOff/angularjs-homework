dba0cedef33801a0fbf985c2169fa54318c46c79
Yavor Atov <yavor.atov@gmail.com>
2014-07-14 22:00:48 +0300

    Added todo rest app

--- day5/rest/bootstrap/public/src/Todo.js
+++ day5/rest/bootstrap/public/src/Todo.js
@@ -1,2 +1,71 @@
 /* global TodoApp */
 
+(function (TodoApp) {
+    'use strict';
+    TodoApp.factory('Todo', function ($http) {
+        var todos = [];
+
+        function Todo(data) {
+            this.id = data.id;
+            this.title = data.title;
+            this.created = data.created;
+            this.until = data.until;
+        }
+
+        Todo.get = function get(id) {
+            return $http({
+                method: 'get',
+                url: '/todo/' + id
+            }).then(function (data) {
+                return new Todo(data.data);
+            });
+        };
+
+        Todo.getList = function getList() {
+            return $http({
+                method: 'get',
+                url: '/todo'
+            }).then(function (data) {
+                return data.data.map(function (d) {
+                    return new Todo(d);
+                });
+            });
+        };
+
+        Todo.prototype.save = function save() {
+            var self = this;
+            $http({
+                method: 'post',
+                url: '/todo',
+                data: {
+                    title: this.title,
+                    created: this.created,
+                    until: this.until,
+                }
+            }).success(function (data, status, headers, config) {
+                self.id = data.id;
+            })
+            .error(function (data, status, headers, config) {
+            });
+        };
+
+        Todo.prototype.destroy = function destroy() {
+            $http({
+                method: 'delete',
+                url: '/todo/' + this.id
+            }).success(function (data, status, headers, config) {
+                todos.splice(todos.indexOf(this), 1);
+            })
+            .error(function (data, status, headers, config) {
+            });
+        };
+
+        Todo.getList().then(function (t) {
+            todos = t;
+        });
+
+        return Todo;
+    });
+}(TodoApp));
+
+
--- day5/rest/bootstrap/public/src/TodoCtrl.js
+++ day5/rest/bootstrap/public/src/TodoCtrl.js
@@ -1,2 +1,32 @@
 /* global TodoApp */
 
+(function (TodoApp) {
+  'use strict';
+  TodoApp.controller('TodoCtrl', function TodoCtrl($scope, Todo) {
+      Todo.getList().then(function (todos) {
+          $scope.todos = todos;
+      });
+      $scope.add = function add() {
+          var todo = new Todo({
+              title: $scope.todoTitle,
+              created: new Date(),
+              until: Date($scope.todoDate + '/' + $scope.todoTime)
+          });
+          todo.save();
+          $scope.todos.push(todo);
+          $scope.todoTitle = '';
+          $scope.todoDate = '';
+          $scope.todoTime = '';
+      };
+      $scope.remove = function remove(todo) {
+          todo.destroy();
+          $scope.todos.splice($scope.todos.indexOf(todo), 1);
+      };
+      $scope.details = function details(todo) {
+          Todo.get(todo.id).then(function (todo) {
+            $scope.todo = todo;
+          });
+      };
+  });
+}(TodoApp));
+
